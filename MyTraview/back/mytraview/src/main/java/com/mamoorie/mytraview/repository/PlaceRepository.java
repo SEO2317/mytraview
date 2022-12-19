bccdb31e6e948601fc2d8613efb5acd8ab6292bb
SEO2317 <107653158+SEO2317@users.noreply.github.com>
2022-12-19 11:49:01 +0900

    Delete MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/repository directory

--- MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/repository/PlaceRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.mamoorie.mytraview.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-////public interface PlaceRepository extends JpaRepository<T, ID> {
-////
-////}
