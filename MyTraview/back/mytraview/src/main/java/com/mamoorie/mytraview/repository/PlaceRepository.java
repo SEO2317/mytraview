9dad21d075caa546901d661407221e8557b0779d
SEO2317 <seoyeong2317@gmail.com>
2022-12-19 11:48:12 +0900

    third

---
+++ MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/repository/PlaceRepository.java
@@ -0,0 +1,7 @@
+//package com.mamoorie.mytraview.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+////public interface PlaceRepository extends JpaRepository<T, ID> {
+////
+////}
