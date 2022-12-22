bfbcbffd894d15fea7d2d63303089a1c3a84c52d
SEO2317 <seoyeong2317@gmail.com>
2022-12-22 16:07:40 +0900

    member working

--- MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/preferences/jwt/JwtTokenProvider.java
+++ MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/preferences/jwt/JwtTokenProvider.java
@@ -1,2 +1,32 @@
-package com.mamoorie.mytraview.preferences.jwt;public class JwtTokenProvider {
+package com.mamoorie.mytraview.preferences.jwt;
+
+import com.mamoorie.mytraview.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+    private final static String SECRET_KEY = "dlrjqjsdurgksmstkfkaqkqhdlazz";
+
+    public String makeJwtToken(User user) {
+        Date revalidated = Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
+
+        return Jwts.builder().signWith(SignatureAlgorithm.HS512, String.valueOf(SECRET_KEY)).setSubject(user.getName())
+                .setIssuer("mytraview app").setIssuedAt(new Date()).setExpiration(revalidated).compact();
+    }
+
+    public String validateAndGetName(String token) {
+        Claims claims = Jwts.parser().setSigningKey(String.valueOf(SECRET_KEY)).parseClaimsJws(token).getBody();
+
+        return claims.getSubject();
+    }
 }
--- MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/preferences/security/WebMvcConfig.java
+++ MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/preferences/security/WebMvcConfig.java
@@ -1,2 +1,14 @@
-package com.mamoorie.mytraview.preferences.security;public class WebMvcConfig {
-}
+//package com.mamoorie.mytraview.preferences.security;
+//
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//public class WebMvcConfig implements WebMvcConfigurer {
+//    private final long MAX_AGE_SECS = 3600;
+//
+//    @Override
+//    public void addCorsMappings(@NotNull CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("http://localhost:3100").allowedHeaders("*")
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowCredentials(true)
+//                .maxAge(MAX_AGE_SECS);
+//    }
+//}
