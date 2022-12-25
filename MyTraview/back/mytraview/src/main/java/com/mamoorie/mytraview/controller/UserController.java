62b1b3fd588a997f68a1d995d7789440db1781fe
이승주(Seungju Lee)/프로모션플랫폼개발팀/11ST <goodguy:@sk.com>
2022-12-25 19:19:35 +0900

    DI 방식을 construct Injection 방식 추천 참고 : https://coding-start.tistory.com/250

--- MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/controller/UserController.java
+++ MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/controller/UserController.java
@@ -11,14 +11,16 @@ import com.mamoorie.mytraview.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
@@ -126,7 +128,7 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<User.Response> LoginUser(@RequestBody User.Request request) {
-        User user = userRepository.findByEmailAndPwd(request.getEmail(), request.getPw());
+        User user = userRepository.findByEmailAndPw(request.getEmail(), request.getPw());
 
         String token = jwtTokenProvider.makeJwtToken(user);
 
--- MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/service/ArticleService.java
+++ MyTraview/back/mytraview/src/main/java/com/mamoorie/mytraview/service/ArticleService.java
@@ -1,18 +1,17 @@
 package com.mamoorie.mytraview.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.mamoorie.mytraview.entity.Article;
 import com.mamoorie.mytraview.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ArticleService {
-	
-	@Autowired
-	private ArticleRepository articleRepository;
+
+	private final ArticleRepository articleRepository;
 	
 	public List<Article> findAllArticles() {
 		return articleRepository.findAll();
